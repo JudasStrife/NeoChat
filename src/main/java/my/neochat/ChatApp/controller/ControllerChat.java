3f30438525bfe330dfa45065cc00b2fe0d17fc89
Judas Strife <judas.striffe@gmail.com>
2025-07-19 19:18:58 +0300

    modified:   src/main/java/my/neochat/ChatApp/config/SecurityFilterConfiguration.java 	modified:   src/main/java/my/neochat/ChatApp/controller/ControllerChat.java 	modified:   src/main/resources/static/styles.css 	modified:   src/main/resources/templates/login.html 	modified:   src/main/resources/templates/signup.html

--- src/main/java/my/neochat/ChatApp/controller/ControllerChat.java
+++ src/main/java/my/neochat/ChatApp/controller/ControllerChat.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import my.neochat.ChatApp.model.ChatMessage;
 import my.neochat.ChatApp.model.ChatUser;
 import my.neochat.ChatApp.service.ServiceChat;
